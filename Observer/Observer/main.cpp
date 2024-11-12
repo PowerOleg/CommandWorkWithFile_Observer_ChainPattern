6cb65dc34f5fbd12409c60bd367de8879406d2f5
PowerOleg <geraskin.oleg@mail.ru>
2024-11-12 16:07:52 +0300

    unresolved external symbol

--- Observer/Observer/main.cpp
+++ Observer/Observer/main.cpp
@@ -1,7 +1,124 @@
 #include <iostream>
-#include <filesystem>
-#include <fstream>
+#include <string>
+//#include <filesystem>
+//#include <fstream>
+#include <memory>
+#include <algorithm>
+#include <vector>
 
+class Observed
+{
+public:
+	virtual ~Observed() = default;
+	virtual void Warning(const std::string& message) const;
+	virtual void Error(const std::string& message) const;
+	virtual void FatalError(const std::string& message) const;
+};
+
+class ObservedChild : public Observed
+{
+public:
+	~ObservedChild() override
+	{}
+
+	void Warning(const std::string& message) const override
+	{
+		std::cout << "Warning: " << message << std::endl;
+	}
+
+	virtual void Error(const std::string& message) const override
+	{
+		std::cout << "Error: " << message << std::endl;
+	}
+
+	void FatalError(const std::string& message) const override
+	{
+		std::cout << "FatalError: " << message << std::endl;
+	}
+
+};
+
+
+
+
+class Observer
+{
+public:
+	virtual ~Observer() = default;
+	virtual void onWarning(const std::string& message) {}
+	virtual void onError(const std::string& message) {}
+	virtual void onFatalError(const std::string& message) {}
+};
+
+class ObserverChild : public Observer
+{
+public:
+	virtual ~ObserverChild() override
+	{}
+
+	void onWarning(const std::string& message)
+	{
+		for (auto observed : observed_) {
+			if (auto strong_ptr = observed.lock()) {
+				strong_ptr->Warning(message);
+			}
+		}
+	}
+
+	void onError(const std::string& message)
+	{
+		for (auto observed : observed_) {
+			if (auto strong_ptr = observed.lock()) {
+				strong_ptr->Error(message);
+			}
+		}
+	}
+
+	void onFatalError(const std::string& message)
+	{
+		for (auto observed : observed_) {
+			if (auto strong_ptr = observed.lock()) {
+				strong_ptr->FatalError(message);
+			}
+		}
+	}
+
+	void AddObserver(std::weak_ptr<Observed> observed)
+	{
+		observed_.push_back(observed);
+	}
+
+private:
+	std::vector<std::weak_ptr<Observed>> observed_;
+};
+
+
+int main(int argc, char** argv)
+{
+	ObserverChild observer;
+
+	ObservedChild observed;
+	std::shared_ptr<ObservedChild> ptr_observed = std::make_shared<ObservedChild>(observed);//std::shared_ptr<ObservedChild> ptr_observed(&observed);	
+	std::weak_ptr<ObservedChild> weak_ptr(ptr_observed);
+	observer.AddObserver(weak_ptr);
+	
+	observer.onWarning("Everybody dance now");
+
+	return 0;
+
+	
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+/*
 class Subscriber
 {
 public:
@@ -68,105 +185,7 @@ int main(int argc, char** argv)
 	group.StopLoading();
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-class LogCommand
-{
-public:
-    virtual ~LogCommand() = default;
-    virtual void print(const std::string& message) = 0;
-};
-
-class LogToConsoleCommand : public LogCommand
-{
-public:
-    void print(const std::string& message) override
-    {
-        std::cout << message << std::endl;
-    }
-};
-
-class LogToFileCommand : public LogCommand
-{
-public:
-    LogToFileCommand(std::string path_) : path{ path_ }
-    {}
-
-    void print(const std::string& message) override
-    {
-        std::ofstream fout(path, std::ios_base::out | std::ios_base::trunc);
-        if (!fout.is_open())
-        {
-            //work with compiler std:c++17
-            int index = path.find_last_of('\\');
-            std::string dir = path.substr(0, index);
-            std::filesystem::create_directories(dir);// #include<windows.h> //std::CreateDirectory("C:\\Users\\morons", NULL);
-            std::cout << "Created directory .\\Command\\log\\ \n";
-            fout = std::ofstream(path, std::ios_base::out | std::ios_base::trunc);
-        }
-        //if (!fout.is_open()){ return;}
-        fout << message;
-        fout.close();
-    }
-private:
-    std::string path = "";
-};
-
-void print(LogCommand& command, const std::string& message)
-{
-    command.print(message);
-}
-
-
-
-class LogObserverParent
-{
-public:
-	virtual ~LogObserverParent() = default;
-	virtual void Warning(const std::string& message) const;
-	virtual void Error(const std::string& message) const;
-	virtual void FatalError(const std::string& message) const;
-};
-
-class Observer
-{
-public:
-	virtual ~Observer() = default;
-	virtual void onWarning(const std::string& message) {}
-	virtual void onError(const std::string& message) {}
-	virtual void onFatalError(const std::string& message) {}
-};
-
 */
-/*int main(int argc, char** argv)
-{
-	//вывод сообщения message в консоль
-	LogToConsoleCommand logToConsole;
-	print(logToConsole, "log to console");
-
-	//вывод сообщения в файл, по указанному пути.
-	LogToFileCommand logToFile(".\\log\\log.txt");//".\\Command\\log.txt");
-	print(logToFile, "log to file");
-
-	//В иерархии классов-наблюдателей нужно реализовать класс для работы с предупреждениями,
-	//который будет печатать сообщение в консоль;
-
-	return 0;
-}*/
-
 
 
 
