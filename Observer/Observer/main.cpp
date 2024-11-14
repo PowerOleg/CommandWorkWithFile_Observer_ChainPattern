1298064cff6ca86f59f6368105a05fb230f6e745
PowerOleg <geraskin.oleg@mail.ru>
2024-11-14 14:52:39 +0300

    must abstract method = 0

--- Observer/Observer/main.cpp
+++ Observer/Observer/main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
 #include <string>
-//#include <filesystem>
-//#include <fstream>
 #include <memory>
 #include <algorithm>
 #include <vector>
 
-class Observed
+class IWatcher
 {
 public:
-	virtual ~Observed() = default;
-	virtual void Warning(const std::string& message) const;
-	virtual void Error(const std::string& message) const;
-	virtual void FatalError(const std::string& message) const;
+	virtual ~IWatcher() = default;
+	virtual void Warning(const std::string& message) const = 0;
+	virtual void Error(const std::string& message) const = 0;
+	virtual void FatalError(const std::string& message) const = 0;
 };
 
-class ObservedChild : public Observed
+class IObserver
 {
 public:
-	~ObservedChild() override
-	{}
+	virtual ~IObserver() = default;
+	virtual void onWarning(const std::string& message) = 0;
+	virtual void onError(const std::string& message) = 0;
+	virtual void onFatalError(const std::string& message) = 0;
+};
+
+class Watcher : public IWatcher
+{
+public:
+	~Watcher() override {}
 
 	void Warning(const std::string& message) const override
 	{
@@ -35,159 +41,56 @@ public:
 	{
 		std::cout << "FatalError: " << message << std::endl;
 	}
-
 };
 
-
-
-
-class Observer
+class Observer : public IObserver
 {
 public:
-	virtual ~Observer() = default;
-	virtual void onWarning(const std::string& message) {}
-	virtual void onError(const std::string& message) {}
-	virtual void onFatalError(const std::string& message) {}
-};
-
-class ObserverChild : public Observer
-{
-public:
-	virtual ~ObserverChild() override
-	{}
-
 	void onWarning(const std::string& message)
 	{
-		for (auto observed : observed_) {
-			if (auto strong_ptr = observed.lock()) {
-				strong_ptr->Warning(message);
-			}
+		for (auto watcher : watchers) {
+//			if (auto strong_ptr = observed.lock()) {
+			watcher->Warning(message);
 		}
 	}
 
 	void onError(const std::string& message)
 	{
-		for (auto observed : observed_) {
-			if (auto strong_ptr = observed.lock()) {
-				strong_ptr->Error(message);
-			}
+		for (auto watcher : watchers) {
+			watcher->Error(message);
 		}
 	}
 
 	void onFatalError(const std::string& message)
 	{
-		for (auto observed : observed_) {
-			if (auto strong_ptr = observed.lock()) {
-				strong_ptr->FatalError(message);
-			}
+		for (auto watcher : watchers) {
+			watcher->FatalError(message);
 		}
 	}
 
-	void AddObserver(std::weak_ptr<Observed> observed)
+	void AddObserver(std::shared_ptr<IWatcher> watcher)
 	{
-		observed_.push_back(observed);
+		watchers.push_back(watcher);
 	}
 
 private:
-	std::vector<std::weak_ptr<Observed>> observed_;
+	std::vector<std::shared_ptr<IWatcher>> watchers;
 };
 
 
 int main(int argc, char** argv)
 {
-	ObserverChild observer;
-
-	ObservedChild observed;
-	std::shared_ptr<ObservedChild> ptr_observed = std::make_shared<ObservedChild>(observed);//std::shared_ptr<ObservedChild> ptr_observed(&observed);	
-	std::weak_ptr<ObservedChild> weak_ptr(ptr_observed);
-	observer.AddObserver(weak_ptr);
-	
+	Observer observer;
+	for (size_t i = 0; i < 4; i++)
+	{
+		Watcher watcher;
+		auto ptr_watcher = std::make_shared<Watcher>(watcher);//std::weak_ptr<SubscriberChild> weak_ptr_subscriber(ptr_subscriber);
+		observer.AddObserver(ptr_watcher);
+	}
 	observer.onWarning("Everybody dance now");
-
-	return 0;
-
-	
-
+	observer.onError("Error dancing");
+	observer.onFatalError("T_T");
 	return 0;
 }
-
-
-
-
-
-
-
-
-/*
-class Subscriber
-{
-public:
-	virtual ~Subscriber() = default;
-	virtual void DidStartLoading() {}
-	virtual void DidStopLoading() {}
-};
-
-class SubscriberChild : public Subscriber
-{
-public:
-	~SubscriberChild() override
-	{
-
-	}
-
-	void DidStartLoading() override
-	{
-		std::cout << "start loading" << std::endl;
-	}
-
-	void DidStopLoading() override
-	{
-		std::cout << "stop loading" << std::endl;
-	}
-};
-
-class ObserverGroup
-{
-public:
-	void StartLoading()
-	{
-		for (auto observer : subscribers_) {
-			if (auto strong_ptr = observer.lock()) {
-				strong_ptr->DidStartLoading();
-			}
-		}
-	}
-
-	void StopLoading()
-	{
-		for (auto observer : subscribers_) {
-			if (auto strong_ptr = observer.lock()) {
-				strong_ptr->DidStopLoading();
-			}
-		}
-	}
-
-	void AddObserver(std::weak_ptr<Subscriber> subscriber)
-	{
-		subscribers_.push_back(subscriber);
-	}
-private:
-	std::vector<std::weak_ptr<Subscriber>> subscribers_;
-};
-
-int main(int argc, char** argv)
-{
-	std::shared_ptr<SubscriberChild> ptr_subscriber = std::make_shared<SubscriberChild>();
-	std::weak_ptr<SubscriberChild> weak_ptr_subscriber(ptr_subscriber);
-	ObserverGroup group;
-	group.AddObserver(weak_ptr_subscriber);
-	group.StartLoading();
-	group.StopLoading();
-	return 0;
-}
-*/
-
-
-
 
 
